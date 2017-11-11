30170a93e7e47fe440cce5511a4570be352916f9
Jihane <Jihane@Jihane-laptop.Home>
2017-11-11 14:40:00 +0000

    [CHECKOUT KATA] Initial Commit

---
+++ checkoutkata/src/main/java/com/itv/checkoutkata/Checkout.java
@@ -0,0 +1,13 @@
+package com.itv.checkoutkata;
+
+/**
+ * Hello world!
+ *
+ */
+public class Checkout 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
